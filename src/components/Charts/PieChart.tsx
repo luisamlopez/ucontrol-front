0bf1778e65fa6cb220deb2515134b388987b356b
Luisa Lopez <lmlopez.19@est.ucab.edu.ve>
2023-06-16 22:20:15 -0400

    Add PieChart

---
+++ src/components/Charts/PieChart.tsx
@@ -0,0 +1,155 @@
+import { KeyboardArrowDownRounded } from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  ClickAwayListener,
+  Grow,
+  MenuItem,
+  MenuList,
+  Paper,
+  Popper,
+} from "@mui/material";
+import { useState, useRef } from "react";
+import {
+  ArgumentAxis,
+  ValueAxis,
+  Chart,
+  PieSeries,
+  Legend,
+} from "@devexpress/dx-react-chart-material-ui";
+
+const options = ["Descargar CSV", "Descargar PDF"];
+const data = [
+  { argument: "Monday", value: 30 },
+  { argument: "Tuesday", value: 20 },
+  { argument: "Wednesday", value: 10 },
+  { argument: "Thursday", value: 50 },
+  { argument: "Friday", value: 60 },
+];
+
+const PieChart = (): JSX.Element => {
+  const [open, setOpen] = useState(false);
+  const anchorRef = useRef<HTMLDivElement>(null);
+  const [selectedIndex, setSelectedIndex] = useState(1);
+  const [boxMarginBottom, setBoxMarginBottom] = useState(0);
+  const handleClick = () => {
+    console.info(`You clicked ${options[selectedIndex]}`);
+  };
+
+  const handleMenuItemClick = (
+    event: React.MouseEvent<HTMLLIElement, MouseEvent>,
+    index: number
+  ) => {
+    setSelectedIndex(index);
+    setOpen(false);
+    setBoxMarginBottom(0);
+    alert("Descargando " + options[index]);
+  };
+
+  const handleToggle = () => {
+    setOpen((prevOpen) => !prevOpen);
+  };
+
+  const handleClose = (event: Event) => {
+    setBoxMarginBottom(0);
+    if (
+      anchorRef.current &&
+      anchorRef.current.contains(event.target as HTMLElement)
+    ) {
+      return;
+    }
+
+    setOpen(false);
+  };
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: {
+          lg: "column",
+          md: "column-reverse",
+          xs: "column-reverse",
+          sm: "column-reverse",
+        },
+        p: 1,
+        mb: boxMarginBottom,
+      }}
+    >
+      <ButtonGroup
+        variant="contained"
+        ref={anchorRef}
+        aria-label="split button"
+        sx={{
+          alignSelf: "flex-end",
+          mb: 2,
+        }}
+      >
+        <Button onClick={handleClick}>{options[selectedIndex]}</Button>
+        <Button
+          size="small"
+          aria-controls={open ? "split-button-menu" : undefined}
+          aria-expanded={open ? "true" : undefined}
+          aria-label="select merge strategy"
+          aria-haspopup="menu"
+          onClick={() => {
+            setBoxMarginBottom(8);
+            handleToggle();
+          }}
+        >
+          <KeyboardArrowDownRounded />
+        </Button>
+      </ButtonGroup>
+      <Popper
+        sx={{
+          zIndex: 1,
+        }}
+        open={open}
+        anchorEl={anchorRef.current}
+        role={undefined}
+        transition
+        disablePortal
+      >
+        {({ TransitionProps, placement }) => (
+          <Grow
+            {...TransitionProps}
+            style={{
+              transformOrigin:
+                placement === "bottom" ? "center top" : "center bottom",
+            }}
+          >
+            <Paper>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList id="split-button-menu" autoFocusItem>
+                  {options.map((option, index) => (
+                    <MenuItem
+                      key={option}
+                      disabled={index === 2}
+                      selected={index === selectedIndex}
+                      onClick={(event) => handleMenuItemClick(event, index)}
+                    >
+                      {option}
+                    </MenuItem>
+                  ))}
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+      <Paper
+        sx={{
+          mb: 2,
+          zIndex: 0,
+        }}
+      >
+        <Chart data={data} height={250}>
+          <Legend />
+          <PieSeries valueField="value" argumentField="argument" />
+        </Chart>
+      </Paper>
+    </Box>
+  );
+};
+
+export default PieChart;
--- src/components/SpaceDeviceDetails.tsx
+++ src/components/SpaceDeviceDetails.tsx
@@ -20,6 +20,7 @@ import { Sidebar } from "./Sidebar";
 import { KeyboardArrowDownRounded } from "@mui/icons-material";
 import DevicesDetailsText from "./DeviceDetailsText";
 import BarChart from "./Charts/BarChart";
+import PieChart from "./Charts/PieChart";
 
 const options = ["Descargar CSV", "Descargar PDF"];
 
@@ -68,38 +69,6 @@ function Details(props: { device: Device }): JSX.Element {
 }
 
 function Graph(this: any, props: { device: Device }): JSX.Element {
-  const [open, setOpen] = useState(false);
-  const anchorRef = useRef<HTMLDivElement>(null);
-  const [selectedIndex, setSelectedIndex] = useState(1);
-
-  const handleClick = () => {
-    console.info(`You clicked ${options[selectedIndex]}`);
-  };
-
-  const handleMenuItemClick = (
-    event: React.MouseEvent<HTMLLIElement, MouseEvent>,
-    index: number
-  ) => {
-    setSelectedIndex(index);
-    setOpen(false);
-    alert("Descargando " + options[index]);
-  };
-
-  const handleToggle = () => {
-    setOpen((prevOpen) => !prevOpen);
-  };
-
-  const handleClose = (event: Event) => {
-    if (
-      anchorRef.current &&
-      anchorRef.current.contains(event.target as HTMLElement)
-    ) {
-      return;
-    }
-
-    setOpen(false);
-  };
-
   return (
     <Box
       sx={{
@@ -118,66 +87,7 @@ function Graph(this: any, props: { device: Device }): JSX.Element {
         borderRadius: "4px",
       }}
     >
-      {/* <ButtonGroup
-        variant="contained"
-        ref={anchorRef}
-        aria-label="split button"
-        sx={{
-          alignSelf: "flex-end",
-        }}
-      >
-        <Button onClick={handleClick}>{options[selectedIndex]}</Button>
-        <Button
-          size="small"
-          aria-controls={open ? "split-button-menu" : undefined}
-          aria-expanded={open ? "true" : undefined}
-          aria-label="select merge strategy"
-          aria-haspopup="menu"
-          onClick={() => {
-            setOpen((prevOpen) => !prevOpen);
-          }}
-        >
-          <KeyboardArrowDownRounded />
-        </Button>
-      </ButtonGroup>
-      <Popper
-        sx={{
-          zIndex: 1,
-        }}
-        open={open}
-        anchorEl={anchorRef.current}
-        role={undefined}
-        transition
-        disablePortal
-      >
-        {({ TransitionProps, placement }) => (
-          <Grow
-            {...TransitionProps}
-            style={{
-              transformOrigin:
-                placement === "bottom" ? "center top" : "center bottom",
-            }}
-          >
-            <Paper>
-              <ClickAwayListener onClickAway={handleClose}>
-                <MenuList id="split-button-menu" autoFocusItem>
-                  {options.map((option, index) => (
-                    <MenuItem
-                      key={option}
-                      disabled={index === 2}
-                      selected={index === selectedIndex}
-                      onClick={(event) => handleMenuItemClick(event, index)}
-                    >
-                      {option}
-                    </MenuItem>
-                  ))}
-                </MenuList>
-              </ClickAwayListener>
-            </Paper>
-          </Grow>
-        )}
-      </Popper> */}
-      <BarChart />
+      <PieChart />
     </Box>
   );
 }
