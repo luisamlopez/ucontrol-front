4c0f9341a37a75a934235379677d4f0eb64285f3
Luisa López <lmlopez.19@est.ucab.edu.ve>
2023-07-27 16:40:29 -0400

    Add autosave hook

--- src/pages/Settings.tsx
+++ src/pages/Settings.tsx
@@ -4,18 +4,22 @@ import {
   Typography,
   CircularProgress,
   Stack,
+  FormControlLabel,
+  FormLabel,
+  Radio,
 } from "@mui/material";
 import HistoryAccordion from "../components/HistoryPage/HistoryAccordion";
 import HistoryTable from "../components/HistoryPage/HistoryTable";
 import { Sidebar } from "../components/Sidebar";
 import { useUser } from "../contexts/authContext";
 import { Field, Form, Formik, FormikHelpers } from "formik";
-import { TextField } from "formik-mui";
+import { RadioGroup, TextField } from "formik-mui";
 import * as yup from "yup";
 import { useSnackbar } from "notistack";
 import { useState } from "react";
 import PasswordField from "../components/Fields/PasswordField";
 import { LoadingButton } from "@mui/lab";
+import useAutosave from "../hooks/useAutosave.hook";
 
 interface ChangePaswordFormValues {
   oldPassword: string;
@@ -23,37 +27,48 @@ interface ChangePaswordFormValues {
   newPasswordConfirmation: string;
 }
 
-const changePasswordInitialValues = {
-  oldPassword: "",
-  newPassword: "",
-  newPasswordConfirmation: "",
-};
-
-const CPValidationSchema = yup.object().shape({
-  oldPassword: yup
-    .string()
-    .required("Ingrese su contraseña actual, por favor")
-    .min(6, "La contraseña debe tener al menos 6 caracteres"),
-  newPassword: yup
-    .string()
-    .required("Ingrese su nueva contraseña, por favor")
-    .min(6, "La contraseña debe tener al menos 6 caracteres"),
-  newPasswordConfirmation: yup
-    .string()
-    .required("Confirme su nueva contraseña, por favor")
-    .oneOf([yup.ref("newPassword")], "Las contraseñas no coinciden"),
-});
+interface NotificationsFormValues {
+  notifications: boolean;
+}
 
 const SettingsPage = (): JSX.Element => {
   const { user } = useUser();
   const { enqueueSnackbar } = useSnackbar();
 
-  const [oldPassword, setOldPassword] = useState<string>("");
-  const [newPassword, setNewPassword] = useState<string>("");
-  const [newPasswordConfirmation, setNewPasswordConfirmation] =
-    useState<string>("");
+  const changePasswordInitialValues = {
+    oldPassword: "",
+    newPassword: "",
+    newPasswordConfirmation: "",
+  };
 
-  const onChangePassword = async (
+  const notificationsInitialValues = {
+    notifications: true,
+  };
+
+  const CPValidationSchema = yup.object().shape({
+    oldPassword: yup
+      .string()
+      .required("Ingrese su contraseña actual, por favor")
+      .min(6, "La contraseña debe tener al menos 6 caracteres"),
+    newPassword: yup
+      .string()
+      .required("Ingrese su nueva contraseña, por favor")
+      .min(6, "La contraseña debe tener al menos 6 caracteres"),
+    newPasswordConfirmation: yup
+      .string()
+      .required("Confirme su nueva contraseña, por favor")
+      .oneOf([yup.ref("newPassword")], "Las contraseñas no coinciden"),
+  });
+
+  const notificationsValidationSchema = yup.object().shape({
+    notifications: yup.boolean().required("Seleccione una opción, por favor"),
+  });
+
+  useAutosave(() => {
+    alert("Se ha guardado su configuración");
+  }, 60 * 1000);
+
+  const onChangePassword = (
     values: ChangePaswordFormValues,
     actions: FormikHelpers<ChangePaswordFormValues>
   ) => {
@@ -65,6 +80,7 @@ const SettingsPage = (): JSX.Element => {
       // const response = await login(values);
       // if (response.success) {
       enqueueSnackbar("response.message", { variant: "success" });
+      window.location.reload();
       // } else {
       //   enqueueSnackbar(response.message, { variant: "error" });
       // }
@@ -73,6 +89,14 @@ const SettingsPage = (): JSX.Element => {
     } finally {
       actions.setSubmitting(false);
     }
+  };
+
+  const onNotifications = (
+    values: NotificationsFormValues,
+    actions: FormikHelpers<NotificationsFormValues>
+  ) => {
+    actions.setSubmitting(true);
+    alert(values.notifications);
   };
 
   return (
@@ -99,7 +123,10 @@ const SettingsPage = (): JSX.Element => {
           </Typography>
           <Box
             sx={{
-              width: "40%",
+              width: {
+                lg: "40%",
+                md: "100%",
+              },
             }}
           >
             <Typography
@@ -124,7 +151,6 @@ const SettingsPage = (): JSX.Element => {
                     component={PasswordField}
                     name="oldPassword"
                     label="Contraseña actual"
-                    type="password"
                     variant="outlined"
                     sx={{ mb: 2 }}
                   />
@@ -132,7 +158,6 @@ const SettingsPage = (): JSX.Element => {
                     component={PasswordField}
                     name="newPassword"
                     label="Nueva contraseña"
-                    type="password"
                     variant="outlined"
                     sx={{ mb: 2 }}
                   />
@@ -140,7 +165,6 @@ const SettingsPage = (): JSX.Element => {
                     component={PasswordField}
                     name="newPasswordConfirmation"
                     label="Confirmar contraseña"
-                    type="password"
                     variant="outlined"
                     sx={{ mb: 2 }}
                   />
@@ -164,65 +188,47 @@ const SettingsPage = (): JSX.Element => {
 
           <Box
             sx={{
-              width: "40%",
+              width: {
+                lg: "40%",
+                md: "100%",
+              },
             }}
           >
-            <Typography
-              sx={{
-                mb: 2,
-                fontSize: "18px",
-                fontStyle: "normal",
-                fontWeight: 500,
-              }}
-            >
-              ¿Desea recibir notificaciones de los espacios a los cuales está
-              suscrito?
-            </Typography>
-
             <Formik
-              initialValues={changePasswordInitialValues}
-              onSubmit={onChangePassword}
-              validationSchema={CPValidationSchema}
+              initialValues={notificationsInitialValues}
+              onSubmit={onNotifications}
+              validationSchema={notificationsValidationSchema}
             >
               {({ isSubmitting }) => (
                 <Stack>
                   <Field
-                    component={PasswordField}
-                    name="oldPassword"
-                    label="Contraseña actual"
-                    type="password"
-                    variant="outlined"
-                    sx={{ mb: 2 }}
-                  />
-                  <Field
-                    component={PasswordField}
-                    name="newPassword"
-                    label="Nueva contraseña"
-                    type="password"
-                    variant="outlined"
-                    sx={{ mb: 2 }}
-                  />
-                  <Field
-                    component={PasswordField}
-                    name="newPasswordConfirmation"
-                    label="Confirmar contraseña"
-                    type="password"
-                    variant="outlined"
-                    sx={{ mb: 2 }}
-                  />
-                  <LoadingButton
-                    type="submit"
-                    variant="contained"
-                    loading={isSubmitting}
-                    loadingIndicator={
-                      <CircularProgress size={24} color="secondary" />
-                    }
-                    sx={{
-                      width: "fit-content",
-                    }}
+                    component={RadioGroup}
+                    name="notifications"
+                    label="Recibir notificaciones"
                   >
-                    Cambiar
-                  </LoadingButton>
+                    <FormLabel
+                      sx={{
+                        my: 2,
+                        fontSize: "18px",
+                        fontStyle: "normal",
+                        fontWeight: 500,
+                        color: "primary.main",
+                      }}
+                    >
+                      ¿Desea recibir notificaciones de los espacios a los cuales
+                      está suscrito?
+                    </FormLabel>
+                    <FormControlLabel
+                      value={true}
+                      control={<Radio />}
+                      label="Si"
+                    />
+                    <FormControlLabel
+                      value={false}
+                      control={<Radio />}
+                      label="No"
+                    />
+                  </Field>
                 </Stack>
               )}
             </Formik>
