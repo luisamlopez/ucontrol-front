aa89804f76ce184bbc7fb97de8cc12c51777de96
Luisa López <lmlopez.19@est.ucab.edu.ve>
2023-08-09 21:38:38 -0400

    Fix url

--- src/api/url.ts
+++ src/api/url.ts
@@ -1,3 +1,3 @@
 //TODO osea yo puse esto aquí pero si no te gusta en este archivo aquí solo, podemos ponerlo en otro lado
-//export const url = "http://25.65.250.117:27017/api/";
+//export const url = "http://25.65.250.117:9000/api/";
 export const url = "http://192.168.0.101:9000/api/";
