82cac561e760b0ce24cc8d031c3de1be67a854f3
Luisa López <lmlopez.19@est.ucab.edu.ve>
2023-09-20 13:20:55 -0400

    Fix  local influx credentials

--- src/api/url.ts
+++ src/api/url.ts
@@ -3,7 +3,12 @@
 export const url = "http://25.65.250.117:9000/api/";
 
 // export const urlInflux = "http://192.168.152.71:8086";
-export const urlInflux = "http://25.65.250.117:8086/";
+// export const urlInflux = "http://25.65.250.117:8086/";
+
+export const urlInflux = "http://192.168.250.6:8086/";
+// export const tokenInflux =
+//   "Fe_oRB6aae5GylUjfUdrSUaXh2ZNsJ4TuaqybEcQRk9EMabi1JqhdqiqizzeVLOh1DfQc2BR_Ig_eRBTq6p9hg==";
+
 export const tokenInflux =
-  "Fe_oRB6aae5GylUjfUdrSUaXh2ZNsJ4TuaqybEcQRk9EMabi1JqhdqiqizzeVLOh1DfQc2BR_Ig_eRBTq6p9hg==";
+  "1l3GXQANZshayNKxIAwYzf7STDUXENIurwKEE-0y0D7kI89-8ExSG0yZNBchTd-pNpllixY2qVv1r6kBUbgDBg==";
 export const orgInflux = "iot-system";
